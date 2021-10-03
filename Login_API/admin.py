e398f1f473627d4f123c98048e60ca0604ba2cee
Charlson Peter <charlson.peter@travidux.in>
2021-10-03 14:54:10 +0530

    OneTimePass model created, register in admin site and added restframework jwt token views in main urls

--- Login_API/admin.py
+++ Login_API/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class OneTimePassAdmin(admin.ModelAdmin):
+    list_display = ['user', 'otp', 'dat_created']
+    search_fields = ['user__username', 'otp']
+admin.site.register(OneTimePass, OneTimePassAdmin)
--- Login_API/models.py
+++ Login_API/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class OneTimePass(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True,)
+    otp = models.IntegerField(blank=True, null=True)
+    dat_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        managed = True
+        db_table = 'one_time_pass'
